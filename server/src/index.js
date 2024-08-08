106ead2630fe14f3ab2055a782ec66f5dc3fb25d
taimoor115 <taimoorhussain985@gmail.com>
2024-08-08 12:32:46 -0700

    Middleware and asyncHandler added

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,15 @@
+import app from "./app.js";
 import connectDatabase from "./db/index.js";
 import dotenv from "dotenv";
 
 
 dotenv.config()
 
-connectDatabase();
+connectDatabase().then(() => { 
+    app.listen(process.env.PORT || 8000, () => { 
+        console.log(`Server is working on PORT ${process.env.PORT}`);
+        
+    })
+}).catch((err) => { 
+    console.log(err);
+});
