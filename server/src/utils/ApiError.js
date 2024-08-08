158efb5ac81a0bd989fab5cccd0660dc84139884
taimoor115 <taimoorhussain985@gmail.com>
2024-08-08 12:46:50 -0700

    Custom Errors and Custom Response

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error { 
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck = "",
+    ) { 
+            super(message)
+            this.statusCode = statusCode,
+            this.date = null,
+            this.success = false,
+            this.message = message,
+            this.errors = errors
+        if (statck) {
+            this.stack = statck
+        } else { 
+            Error.captureStackTrace(this, this.constructor)
+        }
+        
+    }
+}
