36f95bee6f680588002630850d94f0e56e73fea9
taimoor115 <taimoorhussain985@gmail.com>
2024-08-11 12:00:02 -0700

    Router

--- server/src/app.js
+++ server/src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes imports
+import userRouter from "./routes/user.routes.js"
+
+
+// routes
+
+app.use("/api/users", userRouter)
+
 export default app;
--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,17 +1,15 @@
 
-export const asyncHandler = (fn) => async (req, res, next) => { 
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
-
-// export const asyncHandler = (fn) => { 
-//     return (req, res, next) => { 
-//         Promise.resolve(fn(req, res, next)).catch((err) => next(err))
+// export const asyncHandler = (fn) => async (req, res, next) => { 
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
 //     }
 // }
+
+export const asyncHandler = (fn) => (req, res, next) => { 
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
